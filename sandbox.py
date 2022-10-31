7488d44acd35563e32edb8cc149d805f2b7e8604
pranavpshah <pranavpshah2098@gmail.com>
2022-10-30 20:02:02 -0400

    test

--- sandbox.py
+++ sandbox.py
@@ -26,7 +26,7 @@ while(flag):
         flag = False
 
     if(connection):
-        # procedure to take a user command and se telnet to interact with bot
+        # procedure to take a user command and se telnet to interact with robot
         command =  input("Enter command: ")
         if(command.lower() == "exit"):
             flag = False
